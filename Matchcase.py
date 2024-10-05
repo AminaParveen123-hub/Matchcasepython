d9d6b1023727d0d7e35761c474a36f1096d1cb14
AminaParveen123-hub <www.amdigitalmarketing224@gmail.com>
2024-10-06 00:58:46 +0500

    Add files via upload

---
+++ Matchcase.py
@@ -0,0 +1,24 @@
+# Matchcase is like the switch statement in python but we cant use te break statement in it  bcz it runs only that case which it matches
+
+x=int(input("Enter the value of x:"))
+match x:
+  case 0 if x%2==0:
+    print("x is Even")
+  case 1 if x%2!=0:
+    print(" Numbers is odd")
+  case _:
+    print("Number is special")
+   # Topic12
+for i in range(10):
+  print(i)
+# Print table of choice
+num=int(input("Enter the number :"))
+for i in range(1,11):
+print("The table of your selected number is:"              {num}*{i}={num*i}")
+num = int(input("Enter the number :"))
+for i in range(1, 11):
+  print(f"The table of your selected number is: {num} * {i} = {num * i}")
+#   # Now Oh comeon you know very well about while loop
+# just 1 min
+
+
---
+++ while.py
@@ -0,0 +1,8 @@
+number=0
+while(number<5):
+  print("Number is:" ,number)
+  number=number+1
+  # Why you try to stop at a particular loaction 
+  # break mtlb loop ko chodkar nikal jao
+  # continue mtlb iteration ko chor k nikal jao
+  
